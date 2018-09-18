672936dad8228186d96b80936033fcac1dd0c710
Gokkul Nath TS <Gokkulnath@users.noreply.github.com>
2018-09-18 16:25:45 +0530

    fastai-setup-gpu.sh
    fastai-setup-gpu.sh

--- setup.sh
+++ setup.sh
@@ -1,38 +1,43 @@
 #!/usr/bin/env bash
 
+# Get Latest OS Updates and Clean the Older Version
 sudo apt update
 sudo apt install unzip -y
 sudo apt -y autoremove
+
+
+# Add CUDA Repository
 sudo add-apt-repository ppa:graphics-drivers/ppa -y
-sudo apt update
-mkdir downloads
-cd ~/downloads/
 wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
 sudo dpkg -i cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
 sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
 sudo apt update
 sudo apt install cuda -y
+
+# Get CUDNN 9.1 and Install it
 wget http://files.fast.ai/files/cudnn-9.1-linux-x64-v7.tgz
 tar xf cudnn-9.1-linux-x64-v7.tgz
 sudo cp cuda/include/*.* /usr/local/cuda/include/
 sudo cp cuda/lib64/*.* /usr/local/cuda/lib64/
-wget https://repo.continuum.io/archive/Anaconda3-5.0.1-Linux-x86_64.sh
-bash Anaconda3-5.0.1-Linux-x86_64.sh -b
+
+# Get Anaconda and Install 
+wget https://repo.continuum.io/archive/Anaconda2-5.2.0-Linux-x86_64.sh # Might Need update Periodically Last Updated : 18-09-2018
+bash Anaconda2-5.2.0-Linux-x86_64.sh -b
 echo 'export PATH=~/anaconda3/bin:$PATH' >> ~/.bashrc
 export PATH=~/anaconda3/bin:$PATH
 source ~/.bashrc
 
+# Clone Fastai Repo --> Not Mandatory
 cd
 git clone https://github.com/fastai/fastai.git
 cd fastai/
+# Creation of Fast.ai GPU Environment
 conda env create -f environment.yml
+
+# Download Additional Model Weights: resnext,densenet etc..
 wget http://files.fast.ai/models/weights.tgz
 tar -xvf weights.tgz
 rm weights.tgz
-
-# clean up of Downloaded Files
-cd 
-rm -rf downloads
 
 
 # Jupyter Notebook setup 
@@ -41,9 +46,10 @@ echo "c = get_config()  # Get the config object.
 c.IPKernelApp.pylab = 'inline'  # in-line figure when using Matplotlib
 c.NotebookApp.ip = '*'  # Serve notebooks locally.
 c.NotebookApp.open_browser = False  # Do not open a browser window by default when using notebooks." >> $HOME/.jupyter/jupyter_notebook_config.py
-jupass=`python -c "from notebook.auth import passwd; print(passwd())"`
-echo "c.NotebookApp.password = u'"$jupass"'" >> $HOME/.jupyter/jupyter_notebook_config.py
+#jupass=`python -c "from notebook.auth import passwd; print(passwd())"`
+#echo "c.NotebookApp.password = u'"$jupass"'" >> $HOME/.jupyter/jupyter_notebook_config.py
 
+# Enable Jupyter Notebook Widgets
 pip install ipywidgets
 jupyter nbextension enable --py widgetsnbextension --sys-prefix
 
