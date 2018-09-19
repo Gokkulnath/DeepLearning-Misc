f231cb270c7b2b344281d5d9d2cbe01e05211082
Gokkul Nath TS <Gokkulnath@users.noreply.github.com>
2018-09-19 15:09:41 +0530

    Update SampleDataset.sh

--- SampleDataset.sh
+++ SampleDataset.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
-DATASET="the-nature-conservancy-fisheries-monitoring"; #dogscats
 
-dirpath="data/${DATASET}/train/*";
+# Use Cases : Given a Dataset with two or multiple folders containing data instances of respective classes.
+# Required Folder Structure :
+# Train
+#  -> Class1
+#  -> Class2
+#  -> Class3
+#  -> ...
+
+# Usage : Copy the script to the Dataset Directory and then execute ./SampleDataset.sh Data_path sample_count
+
+DATASET=$1; # Train Folder Name 
+sample_count=$2;
+
+dirpath="${DATASET}/train/*";
 
 for dir in ${dirpath}; do
         dname="${dir##*/}"
@@ -9,7 +21,7 @@ for dir in ${dirpath}; do
         mkdir -p data/${DATASET}_sample/valid/${dname}
         mkdir -p data/${DATASET}_sample/train/${dname}
 
-        shuf -n 200 -e data/${DATASET}/train/${dname}/* | xargs -i cp {} data/${DATASET}_sample/train/${dname}
-        shuf -n 100 -e data/${DATASET}/valid/${dname}/* | xargs -i cp {} data/${DATASET}_sample/valid/${dname}
+        shuf -n ${sample_count} -e data/${DATASET}/train/${dname}/* | xargs -i cp {} data/${DATASET}_sample/train/${dname}
+        shuf -n ${sample_count} -e data/${DATASET}/valid/${dname}/* | xargs -i cp {} data/${DATASET}_sample/valid/${dname}
 
 done;
